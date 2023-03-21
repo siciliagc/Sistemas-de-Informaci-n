a99fd2850b3db1b32ff7c78ba0103f8b7f56d7ea
jorge-mtz <coke.martinez.ochoa@gmail.com>
2023-03-21 19:53:08 +0100

    creacion db

--- SistemasInformacion/conexionSqlite3.py
+++ SistemasInformacion/conexionSqlite3.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 def createdb():
     try:
         mi_conexion = sqlite3.connect("ETL_system.db")
@@ -8,10 +9,12 @@ def createdb():
         mi_conexion.close()
     except Exception as ex:
         print(ex)
-def insertDevices():
+def insertDevices(variables):
+    print(variables)
     con = sqlite3.connect("ETL_system.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO devices VALUES ('')")
+    cur.execute('INSERT INTO devices(id,ip,localizacion,responsableNombre,responsableTlfn,responsableRol,analisisPuertosAbiertos,analisisServicios,analisisServiviosInseguros,analisisVulnerabilidades) VALUES (?,?,?,?,?,?,?,?,?,?)',variables)
+    con.commit()
     con.close()
 
 
@@ -21,6 +24,14 @@ def insertAlerts():
     cur.execute("INSERT INTO alerts ")
     con.close()
 
-createdb()
-insertAlerts()
-insertDevices()
+#createdb()
+#insertAlerts()
+file = open("devices.json")
+dictDevices = json.load(file)
+file.close()
+
+leerPuertos = eval("['8080/TCP', '3306/TCP', '3306/UDP']")
+print(leerPuertos)
+for i in dictDevices:
+    #print(i['responsable'])
+    insertDevices((i['id'],i['ip'] ,i['localizacion'],i['responsable']['nombre'],i['responsable']['telefono'],i['responsable']['rol'],str(i['analisis']['puertos_abiertos']),i['analisis']['servicios'],i['analisis']['servicios_inseguros'],i['analisis']['vulnerabilidades_detectadas']))
