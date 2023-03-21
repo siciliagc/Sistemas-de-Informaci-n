77257f41326da0f661a9a1aafb4cafd9bf03b04f
jorge-mtz <coke.martinez.ochoa@gmail.com>
2023-03-21 19:03:06 +0100

    creacion db

--- SistemasInformacion/conexionSqlite3.py
+++ SistemasInformacion/conexionSqlite3.py
@@ -4,7 +4,7 @@ def createdb():
         mi_conexion = sqlite3.connect("ETL_system.db")
         cursor = mi_conexion.cursor()
         #cursor.execute("CREATE TABLE alerts (timestamp date, sid real, msg text, clasificacion text, prioridad real, protocolo text, origen text, destino text, puerto real) ")
-        cursor.execute("CREATE TABLE devices (id text, ip text, localizacion text, responsableNombre text,responsableTlfn text, responsableRol text, analisisPuertosAbiertos text, analisisServicios real, analisisServiviosInseguros real, analisisVulnerabilidades real )")
+        cursor.execute("CREATE TABLE devices (id text, ip text, localizacion text, responsableNombre text,responsableTlfn integer, responsableRol text, analisisPuertosAbiertos text, analisisServicios integer, analisisServiviosInseguros integer, analisisVulnerabilidades integer )")
         mi_conexion.close()
     except Exception as ex:
         print(ex)
