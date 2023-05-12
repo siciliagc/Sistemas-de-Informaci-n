aed18309d194ab62645e1d041498a35dce1da3c2
siciliagc <99964708+siciliagc@users.noreply.github.com>
2023-05-12 11:01:23 +0200

    RandomForest
    Lo tengo que revisar :heart:

--- ejercicio5/randomForestDevices.py
+++ ejercicio5/randomForestDevices.py
@@ -1,7 +1,16 @@
 import json
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import export_graphviz
+import os
 from subprocess import call
+from sys import platform
+import graphviz
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, accuracy_score
+from sklearn.tree import export_graphviz
 
 # Cargamos los datos de los dispositivos:
 
@@ -27,25 +36,21 @@ for i in test_data:
     test_X.append([i['servicios_inseguros']/i['servicios']])
     test_y.append(i['peligroso'])
 
-# Entrenamos el Random Forest:
-clf = RandomForestClassifier(max_depth=2, random_state=0, n_estimators=10)
+clf = RandomForestClassifier(max_depth=5, random_state=0, n_estimators=10)
 clf.fit(train_X, train_y)
 
-# Hacemos predicciones:
-
-y_pred = clf.predict(test_X)
+user_Y_pred = clf.predict(test_X)
 
 for i in range(len(clf.estimators_)):
+    print(i)
     estimator = clf.estimators_[i]
     export_graphviz(estimator,
-                    out_file=f'tree_{i}.dot',
-                    feature_names=['servicios_inseguros / servicios'],
-                    class_names=['No peligroso', 'Peligroso'],
-                    rounded=True,
-                    proportion=False,
-                    precision=2,
-                    filled=True)
-    call(['dot', '-Tpng', f'tree_{i}.dot', '-o', f'tree_{i}.png', '-Gdpi=600'])
+                    out_file='tree.dot',
+                    feature_names=['servicios_inseguros/servicios'],
+                    class_names=['No Peligroso', 'Peligroso'],
+                    rounded=True, proportion=False,
+                    precision=2, filled=True)
+    call(['dot','-Tpng', 'tree.dot','-o','tree' + str(i) + '.png','-Gdpi=600'])
 
 
 
