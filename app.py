ffde6ed93a137f53a76fe638a510f3344d328f3f
yisusrg05 <56789931+yisusrg05@users.noreply.github.com>
2023-05-20 13:37:07 +0200

    ejercicio4

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
 import sqlite3
 import pandas as pd
 import plotly.express as px
@@ -7,6 +7,9 @@ import plotly
 import requests
 import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
+import pdfkit
+import plotly.io as pio
+import plotly.offline as plot
 
 app = Flask(__name__)
 
@@ -18,12 +21,12 @@ devices_df = pd.read_sql_query(
     con)
 vulnerabilities = []
 last_updated_cve = ""
+quantityIP = 10
+quantityDevices = 5
 
 
 @app.route('/')
 def index():
-    quantityIP = 10
-    quantityDevices = 5
     # Get the JSON data for the initial graph
     graphIPJSON = graphIP(quantityIP)
     graphDevicesJSON = graphDevices(quantityDevices)
@@ -34,12 +37,14 @@ def index():
 
 @app.route('/graphIP/<int:quantity>')
 def graphIP(quantity):
+    global quantityIP
+    quantityIP = quantity
     ip_mas_problematicas_df = alerts_df[alerts_df['prioridad'] == 1]
     ip_mas_problematicas_df = ip_mas_problematicas_df.groupby('origen')['sid'].count().reset_index(
         name='numero_alertas')
     ip_mas_problematicas_df.sort_values(by=['numero_alertas'], ascending=False, inplace=True)
 
-    fig = px.bar(ip_mas_problematicas_df.head(quantity), x='origen', y='numero_alertas', barmode='group',
+    fig = px.bar(ip_mas_problematicas_df.head(quantityIP), x='origen', y='numero_alertas', barmode='group',
                  labels=dict(origen="IP", numero_alertas="Número de alertas"))
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -48,10 +53,12 @@ def graphIP(quantity):
 
 @app.route('/graphDevices/<int:quantity>')
 def graphDevices(quantity):
+    global quantityDevices
+    quantityDevices = quantity
     dispositivos_vulnerables_df = devices_df.groupby('id')['numero_vulnerabilidades'].sum().reset_index(
         name='numero_vulnerabilidades')
     dispositivos_vulnerables_df.sort_values(by=['numero_vulnerabilidades'], ascending=False, inplace=True)
-    fig = px.bar(dispositivos_vulnerables_df.head(quantity), x='id', y='numero_vulnerabilidades', barmode='group',
+    fig = px.bar(dispositivos_vulnerables_df.head(quantityDevices), x='id', y='numero_vulnerabilidades', barmode='group',
                  labels=dict(id="Dispositivo", numero_vulnerabilidades="Número de vulnerabilidades"))
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -82,6 +89,47 @@ def vulnerabilities_cve():
             vulnerability["url"] = f"https://cve.circl.lu/cve/{last_10_data[i]['id']}"
             vulnerabilities.append(vulnerability)
         last_updated_cve = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+
+def graphIPpdf():
+    global quantityIP
+    ip_mas_problematicas_df = alerts_df[alerts_df['prioridad'] == 1]
+    ip_mas_problematicas_df = ip_mas_problematicas_df.groupby('origen')['sid'].count().reset_index(
+        name='numero_alertas')
+    ip_mas_problematicas_df.sort_values(by=['numero_alertas'], ascending=False, inplace=True)
+
+    fig = px.bar(ip_mas_problematicas_df.head(quantityIP), x='origen', y='numero_alertas', barmode='group',
+                 labels=dict(origen="IP", numero_alertas="Número de alertas"))
+    # Plot the graph and save it as a static image file
+    graph_file = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
+    plot(fig, filename=graph_file.name, include_plotlyjs=False)
+    graph_file.close()
+
+    return graph_file.name
+
+
+@app.route('/pdf')
+def pdf():
+    vulnerabilities_cve()
+
+    # Generate the graph
+    graph = graphIPpdf()
+
+    # Render the HTML template with the vulnerability information
+    rendered_html = render_template('pdf.html', vulnerabilities=vulnerabilities,
+                                    last_updated_cve=last_updated_cve, graph=graph, quantityIP=quantityIP)
+
+    # Generate the PDF from the rendered HTML using pdfkit
+    pdf_file = pdfkit.from_string(rendered_html, False,
+                                  configuration=pdfkit.configuration(
+                                      wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'))
+
+    # Create a response object with PDF MIME type
+    response = make_response(pdf_file)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename=vulnerabilities.pdf'
+
+    return response
 
 
 @app.route('/api/cve')
