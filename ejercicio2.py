87369afd11816e9d1b239f73284541995bd56861
yisusrg05 <56789931+yisusrg05@users.noreply.github.com>
2023-05-22 09:35:02 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -11,17 +11,12 @@ cur = con.cursor()
 df_alerts = pd.read_sql_query("SELECT * from alerts", con)
 df_devices = pd.read_sql_query("SELECT * from devices", con)
 
-ip_mas_problematicas_df = df_alerts[df_alerts['prioridad'] == 1]
-ip_mas_problematicas_df = ip_mas_problematicas_df.groupby('origen')['sid'].count().reset_index(name='numero_alertas')
-ip_mas_problematicas_df.sort_values(by=['numero_alertas'], ascending=False, inplace=True)
-ip_mas_problematicas_df.head(10).plot(title='Top 10 direcciones IPs más problemáticas', x="origen", y="numero_alertas",
-                                      kind="bar")
-plt.xticks(rotation=30, ha="right", rotation_mode="anchor")
-plt.show()
-
 dispositivos_por_peligrosidad_df = df_devices
+print(dispositivos_por_peligrosidad_df['analisisServiviosInseguros'])
+print(dispositivos_por_peligrosidad_df['analisisServicios'])
 dispositivos_por_peligrosidad_df['ip_peligrosa'] = df_devices['analisisServiviosInseguros'] / df_devices[
     'analisisServicios']
+print(dispositivos_por_peligrosidad_df['ip_peligrosa'])
 dispositivos_peligrosos_df = dispositivos_por_peligrosidad_df[
     dispositivos_por_peligrosidad_df['ip_peligrosa'] > 0.330000]
 dispositivos_peligrosos_df.sort_values(by=['ip_peligrosa'], ascending=False, inplace=True)
@@ -35,7 +30,7 @@ plt.axis('equal')
 plt.title('Top 3 Dispositivos más Peligrosos')
 plt.show()
 
-dispositivos_NO_peligrosos_df = dispositivos_por_peligrosidad_df[
+"""dispositivos_NO_peligrosos_df = dispositivos_por_peligrosidad_df[
     dispositivos_por_peligrosidad_df['ip_peligrosa'] < 0.330000]
 dispositivos_NO_peligrosos_df.sort_values(by=['ip_peligrosa'], ascending=False, inplace=True)
 dispositivos_peligrosos_df.head(3)
@@ -44,4 +39,4 @@ peligrosos = dispositivos_NO_peligrosos_df['ip_peligrosa'].tolist()
 plt.pie(peligrosos, labels=etiquetas, autopct='%1.1f%%')
 plt.axis('equal')
 plt.title('Dispositivos NO Peligrosos')
-plt.show()
+plt.show()"""
